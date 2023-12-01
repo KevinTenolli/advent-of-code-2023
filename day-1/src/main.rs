3020bfa17938127c3aeba97f5397aa6e45fae3c0
Kevin Tenolli <tenollikevin@gmail.com>
2023-12-01 17:15:01 +0100

    use deque

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -23,15 +24,19 @@ where
 }
 
 fn get_line_sum(line: &str) -> i32 {
-    let mut first = 0;
-    let mut last = 0;
+    let mut queue: VecDeque<i32> = VecDeque::new();
     for (_, ch) in line.chars().enumerate() {
         if ch.is_numeric() {
-            if first == 0 {
-                first = ch.to_string().parse().unwrap()
-            }
-            last = ch.to_string().parse().unwrap()
+            queue.push_front(ch.to_string().parse().unwrap());
         }
     }
-    first * 10 + last
+
+    match queue.len() {
+        0 => 0,
+        1 => {
+            let val = queue.pop_back().unwrap();
+            val * 10 + val
+        }
+        _ => queue.pop_back().unwrap() * 10 + queue.pop_front().unwrap(),
+    }
 }
