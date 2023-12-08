8ee452a9d832db75414db92d012f15964ff3b9fa
Kevin Tenolli <tenollikevin@gmail.com>
2023-12-08 19:27:36 +0100

    day 8 p1

--- day-8/src/bin/part1.rs
+++ day-8/src/bin/part1.rs
@@ -1,42 +1,58 @@
 use std::collections::BTreeMap;
 use std::fs::File;
-use std::io;
-use std::io::BufRead;
+use std::io::{self, BufRead};
 
 fn main() {
-    let mut current = String::from("AAA");
+    let (instructions, map) = read_input("./input.txt");
     let end = String::from("ZZZ");
-    let mut counter:u32 = 0;
+    let mut counter = 0;
+    let mut current = String::from("AAA");
+
+    let mut iterator = instructions.chars().into_iter().cycle();
+
+    while current != end {
+        let action = iterator.next().unwrap();
+        let sides = map.get(&current).unwrap();
+
+        match action {
+            'L' => current = sides.0.clone(),
+            'R' => current = sides.1.clone(),
+            _ => continue,
+        };
+
+        counter += 1;
+    }
+
+    println!("{}", counter);
+}
+
+fn read_input(filename: &str) -> (String, BTreeMap<String, (String, String)>) {
     let mut instructions = String::new();
-    let mut map:BTreeMap<String, (String, String)> = BTreeMap::new();
-    if let Ok(file) = File::open("./input.txt") {
+    let mut map: BTreeMap<String, (String, String)> = BTreeMap::new();
+
+    if let Ok(file) = File::open(filename) {
         let reader = io::BufReader::new(file);
-        for (idx,line_result) in reader.lines().enumerate() {
+
+        for (idx, line_result) in reader.lines().enumerate() {
             if let Ok(line) = line_result {
                 if idx == 0 {
                     instructions = line;
-                } else if line.len() > 0 {
+                } else if !line.is_empty() {
                     let data = parse_line(&line);
                     map.insert(data.0, (data.1, data.2));
                 }
             }
         }
     }
-    let mut iterator = instructions.chars().into_iter().cycle();
-    while current != end {
-        let action = iterator.next().unwrap();
-        let sides = map.get(&current).unwrap();
-       match action {
-           'L' => current = sides.0.clone(),
-           'R' => current = sides.1.clone(),
-           _ => continue
-       };
-        counter += 1;
-    }
-    println!("{}", counter);
+
+    (instructions, map)
 }
 
 fn parse_line(line: &str) -> (String, String, String) {
     let alphabetic_chars = line.chars().filter(|char| char.is_alphabetic()).collect::<String>();
-    (alphabetic_chars[0..3].to_string(), alphabetic_chars[3..6].to_string(), alphabetic_chars[6..9].to_string())
+    (
+        alphabetic_chars[0..3].to_string(),
+        alphabetic_chars[3..6].to_string(),
+        alphabetic_chars[6..9].to_string(),
+    )
 }
